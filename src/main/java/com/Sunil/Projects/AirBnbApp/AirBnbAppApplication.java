98d3d2ac34e9b2cd64a99facab8aafe97145727c
sunilkumr988 <sunilkumar20006162@gmail.com>
2026-02-28 10:11:54 +0530

    Initial commit

---
+++ src/main/java/com/Sunil/Projects/AirBnbApp/AirBnbAppApplication.java
@@ -0,0 +1,12 @@
+package com.Sunil.Projects.AirBnbApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+@SpringBootApplication
+public class AirBnbAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AirBnbAppApplication.class, args);
+	}
+
+}
